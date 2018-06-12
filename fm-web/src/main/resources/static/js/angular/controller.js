bb6de0c04c182a8c0781bf90635a3360d02c9d8c
JKevinLiu <JKevinLiu@users.noreply.github.com>
2018-06-12 23:00:07 +0800

    产品界面前台功能-20180612

--- fm-web/src/main/resources/static/js/angular/controller.js
+++ fm-web/src/main/resources/static/js/angular/controller.js
@@ -2,19 +2,22 @@ mainApp.controller("initController", ["$rootScope", "userService", function($roo
     // 获取用户数据
     var promise = userService.getUser();
 
+
     promise.then(function(data) {
         $rootScope.user = data;
         $rootScope.role = $rootScope.user.role;
         $rootScope.permissions = $rootScope.user.role.permissionList;
     }, function(data) {
-       //fail
+        //fail
         alert(data);
     });
 }]);
 
+
 mainApp.controller("viewController", ["$scope", "$routeParams", function($scope, $routeParams) {
     //$scope.code = $routeParams.code;
 }]);
+
 
 mainApp.controller("orderController", ["$scope","$http", "$location", function($scope, $http, $location) {
     $http.get("order/list").then(
@@ -27,24 +30,30 @@ mainApp.controller("orderController", ["$scope","$http", "$location", function($
         }
     );
 
+
     $scope.add = function(){
         $location.path("subnav/add_order");
     };
 
+
     $scope.del = function(){
         alert("del");
     };
+
 
     $scope.query = function(){
         alert("query");
     }
 }]);
 
+
 mainApp.controller("orderOptionController", ["$scope", "$routeParams", function($scope, $routeParams) {
     $scope.option = $routeParams.option;
 
+
     //定义数组
     $scope.items = [];
+
 
     //添加的方法
     $scope.add = function(){
@@ -59,6 +68,7 @@ mainApp.controller("orderOptionController", ["$scope", "$routeParams", function(
         $scope.items.push(item);
     }
 
+
     //删除一行
     $scope.dele =function($index){
         $scope.items.splice($index, 1);
@@ -66,8 +76,16 @@ mainApp.controller("orderOptionController", ["$scope", "$routeParams", function(
 }]);
 
 
+
+
 mainApp.controller("productController", ["$scope", "$http", function($scope, $http) {
     $scope.right_title = "添加产品";
+    $scope.show_product = [];
+    $scope.right_product_code = "";
+    $scope.right_product_name = "";
+    $scope.right_alias_select_edit = true;
+    $scope.left_alias_select = "-1";
+    $scope.right_alias_select = "-1";
 
     $scope.getAilas = function(){
         $http.get("product/alias").then(
@@ -79,6 +97,7 @@ mainApp.controller("productController", ["$scope", "$http", function($scope, $ht
             }
         );
     };
+
 
     $scope.getAliasAndchilren = function(aliasId){
         if(aliasId == null){
@@ -94,13 +113,79 @@ mainApp.controller("productController", ["$scope", "$http", function($scope, $ht
         );
     }
 
+
     $scope.change = function(){
         $scope.getAliasAndchilren($scope.left_alias_select);
     }
 
-    $scope.editProduct = function(product){
-        $scope.right_title = "修改产品";
+
+    $scope.editProduct = function(product, index){
+        var flag = false;
+        var ck_products = $("input[name='ck_product']");
+
+        for (var i = 0; i < ck_products.length; i++) {
+            if(i == index && ck_products[i].checked){  //判断是否选中
+                $scope.right_title = "修改产品";
+                $scope.show_product = product;
+                $scope.right_alias_select = product.parentId +"";
+
+                if(product.parentId == "-1"){
+                    $scope.right_alias_select_edit = false;
+                }else{
+                    $scope.right_alias_select_edit = true;
+                }
+
+                flag = true;
+
+            }else{
+                if(ck_products[i].checked){
+                    ck_products[i].checked = false;
+                }
+            }
+        }
+
+        if(flag){
+            return;
+        }
+
+        $scope.show_product = [];
+        $scope.right_alias_select = "-1";
+        $scope.right_title = "添加产品";
+        $scope.right_alias_select_edit = true;
     }
+
+
+    $scope.commit = function(){
+        $http.put("product/save",$scope.show_product).then(
+            function(res){
+                alert(res.data.result);
+                if($scope.right_alias_select != -1){
+                    $scope.getAliasAndchilren($scope.right_alias_select);
+                }
+            },function(res){
+                alert(res.data.result);
+            }
+        );
+    }
+
+
+    $scope.del = function(id){
+        if(!confirm("确定删除该条记录吗？")){
+            return;
+        }
+
+        $http.delete("product/del",id).then(
+            function(res){
+                alert(res.data.result);
+                if($scope.right_alias_select != -1){
+                    $scope.getAliasAndchilren($scope.right_alias_select);
+                }
+            },function(res){
+                alert(res.data.result);
+            }
+        );
+    }
+
 
     $scope.getAilas();
 }]);
